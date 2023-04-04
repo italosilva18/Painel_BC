75d02cf80a05218d45a4821aae0b5460ba0c855c
italosilva18 <italosilva14@hotmail.com>
2023-04-04 14:02:22 -0300

    configuração das rotas, views, e template

--- projetobc/settings.py
+++ projetobc/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registro',
+    #'members',
+    #'bootstrap5',
+    
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'projetobc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
