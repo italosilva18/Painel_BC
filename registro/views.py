ada2606366b0273599279bf72d922a4ca52433ec
italosilva18 <italosilva14@hotmail.com>
2023-04-11 17:17:35 -0300

    show

--- registro/views.py
+++ registro/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'registro/home.html')
+    return render(request, 'registro/pages/home.html')
