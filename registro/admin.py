108307dfa449282ac85b6b337dbec4cac1747c9c
italosilva18 <italosilva14@hotmail.com>
2023-06-06 00:38:59 -0300

    Criação das tabelas com model e configução do painel admin

--- registro/admin.py
+++ registro/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import RegistroAtividade, Equipamento, Funcionario, Funcao, InspecaoEquipamento, RegistroEvento
 
-# Register your models here.
+@admin.register(RegistroAtividade)
+class RegistroAtividadeAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Equipamento)
+class EquipamentoAdmin(admin.ModelAdmin ):
+    ...
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Funcao)
+class FuncaoAdmin(admin.ModelAdmin ):
+    ...
+@admin.register(InspecaoEquipamento)
+class InspecaoEquipamentoAdmin(admin.ModelAdmin ):
+    ...
+    
+@admin.register(RegistroEvento)
+class RegistroEventoAdmin(admin.ModelAdmin ):
+    ...
--- registro/models.py
+++ registro/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Funcao(models.Model):
+    nome = models.CharField(max_length=100)
+
+class Funcionario(User):
+    idade = models.IntegerField()
+    funcao = models.ForeignKey(Funcao, on_delete=models.CASCADE)
+
+class Equipamento(models.Model):
+    nome = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    validade = models.DateField()
+    informacoes = models.TextField()
+    observacao = models.TextField()
+
+class InspecaoEquipamento(models.Model):
+    equipamento = models.ForeignKey(Equipamento, on_delete=models.CASCADE)
+    inspecao = models.TextField()
+    observacao = models.TextField()
+    data_inspecao = models.DateField(auto_now=True)
+    
+class RegistroEvento(models.Model):
+    hora_inicial_evento = models.DateTimeField()
+    hora_final_evento = models.DateTimeField()
+    ronda_interna = models.BooleanField()
+    ronda_externa = models.BooleanField()
+    visitas = models.TextField()
+    manobras = models.TextField()
+    imagens_videos = models.FileField(upload_to='registro/uploads/%Y-%m-%d')
+    
+
+class RegistroAtividade(models.Model):
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
+    numero_ordem_servico = models.IntegerField()
+    data_registro = models.DateField()
+    turno_atual = models.CharField(max_length=100)
+    evento = models.ManyToManyField(RegistroEvento)
+    inspecoes_equipamentos = models.ManyToManyField(InspecaoEquipamento)
+    evento_automatico = models.BooleanField(default=False)
+
+    
+
+    
+    
+
+
+    
